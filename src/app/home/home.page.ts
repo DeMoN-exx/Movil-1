e9b65eadcc879f10118cb9d63653f48df3705cc7
Q-Quinn <joaquinpin@outlook.com>
2023-10-18 21:39:04 -0300

    Animation Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef, ViewChildren, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+
+import type { QueryList } from '@angular/core';
+import type { Animation } from '@ionic/angular';
+import { AnimationController, IonCard } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +13,28 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class HomePage {
   state:any;
   user:any;
-  constructor(private activeroute: ActivatedRoute, private router: Router) {
+
+  @ViewChild(IonCard, { read: ElementRef })
+  card!: ElementRef<HTMLIonCardElement>;
+
+  private animation!: Animation;
+  constructor(private activeroute: ActivatedRoute, private router: Router, private animationCtrl: AnimationController) {
     this.activeroute.queryParams.subscribe(params => {
       this.state = this.router.getCurrentNavigation()?.extras.state;
       this.user = this.state.user
       console.log(this.user);
     })
+  }
+  ngAfterViewInit() {
+    const animation = this.animation = this.animationCtrl
+      .create()
+      .addElement(this.card.nativeElement)
+      .duration(1500)
+      .iterations(Infinity)
+      .fromTo('transform', 'translateX(0px)', 'translateX(100px)')
+      .fromTo('opacity', '1', '0.2');
+    animation.play();
+
   }
 
 }
