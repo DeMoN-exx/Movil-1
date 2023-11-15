03c69ae1acf83cd3c939d755fcd5ada5027c4fbd
DeMoN-exx <lucaslopezh3@gmail.com>
2023-11-14 21:49:03 -0300

    commit roles

--- ApiMovil/ApiProject/ApiProject/settings.py
+++ ApiMovil/ApiProject/ApiProject/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '12345',
         'NAME': 'movil_bd',
     },
 }
--- ApiMovil/ApiProject/core/models.py
+++ ApiMovil/ApiProject/core/models.py
@@ -2,7 +2,20 @@ from django.db import models
 
 # Create your models here.
 class usuario(models.Model):
-    username = models.CharField(max_length=30, primary_key=True)
+    username = models.CharField(max_length=30, primary_key=True, unique=True)
     password = models.CharField(max_length=30)
+
+    #roles
+    ADMIN = 1
+    CLIENTE = 2
+    CHOFER = 3
+    ELEGIR_ROL = (
+        (ADMIN, 'Administrador'),
+        (CLIENTE, 'Cliente'),
+        (CHOFER, 'Chofer'),
+    )
+    role = models.PositiveSmallIntegerField(choices=ELEGIR_ROL,blank=True,null=True)
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['password', 'role']
     def __str__(self):
         return self.username
