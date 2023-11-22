9b0243e56cad0c7d7aa8e1db6b34812bd89f56a6
Q-Quinn <joaquinpin@outlook.com>
2023-11-22 19:35:15 -0300

    Roles en HTML Home, Boton salir y routing fix

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component,ElementRef, ViewChildren, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChildren, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import type { QueryList } from '@angular/core';
@@ -11,45 +11,34 @@ import { AnimationController, IonCard, IonSegment } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  state:any;
-  user:any;
+  state: any;
+  user: any;
+  role: any;
 
   @ViewChild(IonCard, { read: ElementRef })
   card!: ElementRef<HTMLIonCardElement>;
-  @ViewChild(IonSegment) 
-  segment!:IonSegment;
+  @ViewChild(IonSegment)
+  segment!: IonSegment;
 
   private animation!: Animation;
   public selectedSegment!: string
   constructor(private activeroute: ActivatedRoute, private router: Router, private animationCtrl: AnimationController,) {
-    this.activeroute.queryParams.subscribe(params => {
-      this.state = this.router.getCurrentNavigation()?.extras.state;
-      this.user = localStorage.getItem('username')
-      console.log(this.user);
-    })
+    this.state = this.router.getCurrentNavigation()?.extras.state;
+    this.user = localStorage.getItem('username')
+    this.role = localStorage.getItem('role')
+    console.log(this.user);
   }
-  ngAfterViewInit() {
-    const animation = this.animation = this.animationCtrl
-      .create()
-      .addElement(this.card.nativeElement)
-      .duration(1500)
-      .iterations(Infinity)
-      .fromTo('transform', 'translateX(0px)', 'translateX(100px)')
-      .fromTo('opacity', '1', '0.2');
-    animation.play();
-
-  }
-  salir(){
+  salir() {
     localStorage.removeItem('ingresado');
     localStorage.removeItem('username');
     localStorage.removeItem('role');
     this.router.navigate(['/login']);
   }
-  ngoninit(){
+  ngoninit() {
     this.segment.value = 'Inicio';
   }
-  segmentChanged( event: any )  {
-    console.log (event.target.value);
+  segmentChanged(event: any) {
+    console.log(event.target.value);
     this.selectedSegment = event.target.value;
-  } 
+  }
 }
